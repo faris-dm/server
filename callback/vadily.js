a0ea260ea63a9799830fb3c3d6c41ef2f939871c
faris Naser <solonaser9@gmail.com>
2026-01-06 17:41:36 -0800

    feat:create a get route  to fetch all elements in  the aray

--- callback/vadily.js
+++ callback/vadily.js
@@ -32,7 +32,6 @@ const app = express();
 //   },
 // ];
 const MovieList = {
-  type: ["horror", "Action", "Love", "Sci-Fi"],
   horror: ["The Conjuring", "IT", "A Quiet Place", "Hereditary", "Scream"],
   Action: ["john Wick", "Mad Max", "The Dark Night"],
   Love: [
@@ -44,13 +43,29 @@ const MovieList = {
   ],
   "Sci-Fi": ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
 };
+const MenuMovies = ["horror", "Action", "Love", "Sci-Fi"];
 
 app.get("/", (req, res) => {
   res.send({ message: "wellcome   to home pages" });
 });
 
 app.get("/movies", (req, res) => {
-  res.send(MovieList.type);
+  res.send(MenuMovies);
+});
+app.get("/movies/Adventure", (req, res) => {
+  res.send(MovieList["Sci-Fi"]);
+});
+
+app.get("/movies/action", (req, res) => {
+  res.send(MovieList.Action);
+});
+
+app.get("/movies/horror", (req, res) => {
+  res.send(MovieList.horror);
+});
+
+app.get("/movies/love", (req, res) => {
+  res.send(MovieList.Love);
 });
 
 const port = 7600;
