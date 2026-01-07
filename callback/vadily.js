fb0098b72cbfe3eeab11ac658bd5003007f322a5
faris Naser <solonaser9@gmail.com>
2026-01-07 03:29:39 -0800

    feat(array):added a multi dimational array  to fetch data from it

--- callback/vadily.js
+++ callback/vadily.js
@@ -31,18 +31,62 @@ const app = express();
 //     movies: ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
 //   },
 // ];
-const MovieList = {
-  horror: ["The Conjuring", "IT", "A Quiet Place", "Hereditary", "Scream"],
-  Action: ["john Wick", "Mad Max", "The Dark Night"],
-  Love: [
-    "The Notebook",
-    "About Time",
-    "La La Land",
-    "Titanic",
-    "Before Sunrise",
-  ],
-  "Sci-Fi": ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
-};
+// const MovieList = {
+//   horror: ["The Conjuring", "IT", "A Quiet Place", "Hereditary", "Scream"],
+//   Action: ["john Wick", "Mad Max", "The Dark Night"],
+//   Love: [
+//     "The Notebook",
+//     "About Time",
+//     "La La Land",
+//     "Titanic",
+//     "Before Sunrise",
+//   ],
+//   "Sci-Fi": ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
+// };
+const listMovies = [
+  {
+    horror: [
+      {
+        id: 1,
+        title: "The Conjuring",
+        description: "A community of horrors",
+        duration: "2h:42m:2s",
+      },
+      {
+        id: 2,
+        title: "IT",
+        description: "Tech related Viresus",
+        duration: "2h 3s",
+      },
+      {
+        id: 3,
+        title: "A Quiet Place",
+        description: "Old community tring to servive with  witchs",
+        duration: "3h:30m",
+      },
+    ],
+    Action: [
+      {
+        id: 1,
+        title: "The Notebook",
+        description: "Intilgeance campany to find the note books",
+        duration: "2h:43s",
+      },
+      {
+        id: 2,
+        title: "Mad Max",
+        description: "Man to mant to be  Rich",
+        duration: "1h:43s",
+      },
+    ],
+    Adventure: {
+      id: 1,
+      title: "Inception",
+      description: " finding your self",
+      duration: "2h:30m:2s",
+    },
+  },
+];
 const MenuMovies = ["horror", "Action", "Love", "Sci-Fi"];
 
 app.get("/", (req, res) => {
@@ -58,6 +102,14 @@ app.get("/movies/Adventure", (req, res) => {
 
 app.get("/movies/action", (req, res) => {
   res.send(MovieList.Action);
+});
+app.get("/movies/:id", (req, res) => {
+  const IdMovie = MenuMovies.find((item) => item.id == parseInt(req.params.id));
+  if (!IdMovie) {
+    res.send({ Message: "Movie with  that id does not exist" });
+  } else {
+    res.send(MovieList);
+  }
 });
 
 app.get("/movies/horror", (req, res) => {
