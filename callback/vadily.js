70a4d10ec406dd3e39c44af71a1d23bd036375ca
faris Naser <solonaser9@gmail.com>
2026-01-06 17:12:38 -0800

    feat(api):added a  get route to featch data from the list

--- callback/vadily.js
+++ callback/vadily.js
@@ -1,43 +1,56 @@
 const express = require("express");
 
 const app = express();
-const listMovies = [
-  {
-    catagores: "Horor",
-    Movies: ["The Conjuring", "IT", "A Quiet Place", "Hereditary", "Scream"],
-  },
-  {
-    catagores: "Love",
-    Movies: [
-      "The Notebook",
-      "About Time",
-      "La La Land",
-      "Titanic",
-      "Before Sunrise",
-    ],
-  },
-  {
-    catagores: "Action",
-    Movies: [
-      "John Wick",
-      "Mad Max",
-      "The Dark Knight",
-      "Die Hard",
-      "Gladiator",
-    ],
-  },
-  {
-    category: "Sci-Fi",
-    movies: ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
-  },
-];
+// const listMovies = [
+//   {
+//     catagores: "Horor",
+//     Movies: ["The Conjuring", "IT", "A Quiet Place", "Hereditary", "Scream"],
+//   },
+//   {
+//     catagores: "Love",
+//     Movies: [
+//       "The Notebook",
+//       "About Time",
+//       "La La Land",
+//       "Titanic",
+//       "Before Sunrise",
+//     ],
+//   },
+//   {
+//     catagores: "Action",
+//     Movies: [
+//       "John Wick",
+//       "Mad Max",
+//       "The Dark Knight",
+//       "Die Hard",
+//       "Gladiator",
+//     ],
+//   },
+//   {
+//     category: "Sci-Fi",
+//     movies: ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
+//   },
+// ];
+const MovieList = {
+  type: ["horror", "Action", "Love", "Sci-Fi"],
+  horror: ["The Conjuring", "IT", "A Quiet Place", "Hereditary", "Scream"],
+  Action: ["john Wick", "Mad Max", "The Dark Night"],
+  Love: [
+    "The Notebook",
+    "About Time",
+    "La La Land",
+    "Titanic",
+    "Before Sunrise",
+  ],
+  "Sci-Fi": ["Inception", "Interstellar", "The Matrix", "Dune", "Arrival"],
+};
 
 app.get("/", (req, res) => {
   res.send({ message: "wellcome   to home pages" });
 });
 
 app.get("/movies", (req, res) => {
-  res.send(listMovies.catagores);
+  res.send(MovieList.type);
 });
 
 const port = 7600;
