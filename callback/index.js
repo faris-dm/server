ea774579ec163ac541379119aadddfef99aa09af
faris Naser <solonaser9@gmail.com>
2026-01-15 03:40:10 -0800

    feat:added a real ejs file and fixed   the render logic also added a tailwindcss

--- callback/index.js
+++ callback/index.js
@@ -1,15 +1,29 @@
-const { name } = require("ejs");
-const express = require("express");
+// const express = require("express");
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
+
+// app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.render("hello", { name: "Geeks" });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is Running on https//:${port}`);
+// });
+
+let express = require("express");
+let app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("hello", { name: "Geeks" });
+  res.render("hello", { name: "solo" });
 });
 
+let port = 6500;
 app.listen(port, () => {
-  console.log(`Server is Running on https//:${port}`);
+  console.log(`Server is running on http//localhost:${port}`);
 });
